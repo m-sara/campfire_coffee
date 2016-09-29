4d8a86cfb3ed86fbe21f48d13ce21f291fd23375
Maithrika Sara <akirhtiam@gmail.com>
2016-09-28 19:32:03 -0700

    added working logic for random customer, cups per hour, lbs per hour, and employees per hour

--- back.js
+++ back.js
@@ -7,33 +7,59 @@ var Pike = {
   averageLbs: 0.34,
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm'],
   randomCust: [],
+  totalCust: 0,
   cupsPerHour: [],
+  totalCups: 0,
+  lbsPerHour: [],
+  totalLbs: 0,
+  emplPerHour: [],
 
-  randomCustf: function() {
+  getRandomCust: function() {
+    this.randomCust.length = 0;
     for (var i = 0; i < this.hours.length; i++) {
-      var randomCustv = Math.floor((Math.random() * (this.maxCust - this.minCust)) + this.minCust);
-      console.log(randomCustv);
-      this.randomCust.push(randomCustv);
+      var numRandCust = Math.floor((Math.random() * (this.maxCust - this.minCust)) + this.minCust);
+      console.log('Hour ' + (i + 1) + ': ' + numRandCust);
+      this.totalCust += numRandCust;
+      console.log('Total customers: ' + this.totalCust);
+      this.randomCust.push(numRandCust);
+    }
+  },
+  getCupsPerHr: function() {
+    this.cupsPerHour.length = 0;
+    for (var i = 0; i < this.hours.length; i++) {
+      var numCupsHr = Math.ceil(this.randomCust[i] * this.averageCups);
+      console.log('Hour ' + (i + 1) + ': ' + numCupsHr);
+      this.totalCups += numCupsHr;
+      console.log('Total cups: ' + this.totalCups);
+      this.cupsPerHour.push(numCupsHr);
+    }
+  },
+
+  getLbsPerHr: function() {
+    this.lbsPerHour.length = 0;
+    for (var i = 0; i < this.hours.length; i++) {
+      var numLbsHr = Math.ceil(this.randomCust[i] * this.averageLbs);
+      console.log('Hour ' + (i + 1) + ': ' + numLbsHr);
+      this.totalLbs += numLbsHr;
+      console.log('Total lbs: ' + this.totalLbs);
+      this.lbsPerHour.push(numLbsHr);
+    }
+  },
+  getEmployees: function() {
+    for (var i = 0; i < this.hours.length; i++) {
+      var employeeNumber = Math.ceil((this.randomCust[i] * 2) / 60);
+      console.log('Employees for hour ' + (i + 1) + ': ' + employeeNumber);
+      this.emplPerHour.push(employeeNumber);
     }
   }
-  // ,
-  // cupsPerHourf: function() {
-  //   var cupsPerHourv = this.randomCust[i] * this.averageCups;
-  //   console.log(cupsPerHourv);
-  //   return cupsPerHourv;
-  // },
-  // lbsPerHourf: function() {
-  //   var lbsPerHourv = this.randomCust * averageLbs;
-  //   console.log(lbsPerHour);
-  //   return lbsPerHour;
-  // },
-  // // employeeNumber: function() {
-  // //   var employeeNumber = Math.round()
-  // // }
-  // }
 }
 
 
-Pike.randomCustf();
-// Pike.cupsPerHourf();
+Pike.getRandomCust();
 console.log(Pike.randomCust);
+Pike.getCupsPerHr();
+console.log(Pike.cupsPerHour);
+Pike.getLbsPerHr();
+console.log(Pike.lbsPerHour);
+Pike.getEmployees();
+console.log(Pike.emplPerHour);
