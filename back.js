8bb59513bee308ab516db11891ecc257e381a6a0
Maithrika Sara <akirhtiam@gmail.com>
2016-09-28 20:33:15 -0700

    working on beans per hour, total beans, and do all the things

--- back.js
+++ back.js
@@ -12,25 +12,30 @@ var Pike = {
   totalCups: 0,
   lbsPerHour: [],
   totalLbs: 0,
+  cupBeansPerHour: [],
+  totalBeansHr: [],
+  allBeans: 0,
   emplPerHour: [],
+
 
   getRandomCust: function() {
     this.randomCust.length = 0;
     for (var i = 0; i < this.hours.length; i++) {
       var numRandCust = Math.floor((Math.random() * (this.maxCust - this.minCust)) + this.minCust);
-      console.log('Hour ' + (i + 1) + ': ' + numRandCust);
+      console.log('Customers for hour ' + (i + 1) + ': ' + numRandCust);
       this.totalCust += numRandCust;
       console.log('Total customers: ' + this.totalCust);
       this.randomCust.push(numRandCust);
     }
   },
+
   getCupsPerHr: function() {
     this.cupsPerHour.length = 0;
     for (var i = 0; i < this.hours.length; i++) {
       var numCupsHr = Math.ceil(this.randomCust[i] * this.averageCups);
-      console.log('Hour ' + (i + 1) + ': ' + numCupsHr);
+      console.log('Cups for hour ' + (i + 1) + ': ' + numCupsHr);
       this.totalCups += numCupsHr;
-      console.log('Total cups: ' + this.totalCups);
+      console.log('Total cups for the day: ' + this.totalCups);
       this.cupsPerHour.push(numCupsHr);
     }
   },
@@ -39,12 +44,33 @@ var Pike = {
     this.lbsPerHour.length = 0;
     for (var i = 0; i < this.hours.length; i++) {
       var numLbsHr = Math.ceil(this.randomCust[i] * this.averageLbs);
-      console.log('Hour ' + (i + 1) + ': ' + numLbsHr);
+      console.log('Lbs for hour ' + (i + 1) + ': ' + numLbsHr);
       this.totalLbs += numLbsHr;
-      console.log('Total lbs: ' + this.totalLbs);
+      console.log('Total lbs for the day: ' + this.totalLbs);
       this.lbsPerHour.push(numLbsHr);
     }
   },
+
+  getBeansPerHr: function() {
+    this.cupBeansPerHour.length = 0;
+    for (var i = 0; i < this.hours.length; i++) {
+      var numBeenzHr = parseFloat((this.cupsPerHour[i] / 16).toFixed(1));
+      console.log('Lbs of beans for cups in hour ' + (i + 1) + ': ' + numBeenzHr);
+      this.cupBeansPerHour.push(numBeenzHr);
+    }
+  },
+
+  getTotalBeans: function() {
+    for (var i = 0; i < this.hours.length; i++) {
+      console.log(this.cupBeansPerHour[i]);
+      console.log(this.lbsPerHour[i]);
+      var totalBeenz = parseFloat(this.cupBeansPerHour[i] + this.lbsPerHour[i]).toFixed(1);
+      console.log('Total beans for hour ' + (i + 1) + ': ' + totalBeenz);
+      this.totalBeansHr.push(totalBeenz);
+      this.allBeans += parseFloat(totalBeenz);
+    }
+  },
+
   getEmployees: function() {
     for (var i = 0; i < this.hours.length; i++) {
       var employeeNumber = Math.ceil((this.randomCust[i] * 2) / 60);
@@ -54,12 +80,18 @@ var Pike = {
   }
 }
 
-
-Pike.getRandomCust();
-console.log(Pike.randomCust);
-Pike.getCupsPerHr();
-console.log(Pike.cupsPerHour);
-Pike.getLbsPerHr();
-console.log(Pike.lbsPerHour);
-Pike.getEmployees();
-console.log(Pike.emplPerHour);
+doAllTheThings: function() {
+  for (var i = 0; i < this.hours.length; i++) {
+    Pike.getRandomCust();
+    console.log(Pike.randomCust);
+    Pike.getCupsPerHr();
+    console.log('Total cups for the day: ' + Pike.totalCups);
+    console.log(Pike.cupsPerHour);
+    Pike.getLbsPerHr();
+    console.log(Pike.lbsPerHour);
+    Pike.getBeansPerHr();
+    console.log(Pike.cupBeansPerHour);
+    Pike.getTotalBeans();
+    console.log('Total beans total: ' + Pike.allBeans);
+    Pike.getEmployees();
+    console.log(Pike.emplPerHour);
