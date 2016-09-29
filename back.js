9916d85c25ed53a75455f41ce3cd4b1e48a8e632
Maithrika Sara <akirhtiam@gmail.com>
2016-09-28 18:44:53 -0700

    initial commit

---
+++ back.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Pike = {
+  minCust: 14,
+  maxCust: 35,
+  averageCups: 1.2,
+  averageLbs: 0.34,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm'],
+  randomCust: [],
+  cupsPerHour: [],
+
+  for (var i = 0; i < this.hours.length; i++) {
+    randomCustf: function() {
+      var randomCustv = Math.floor((Math.random() * (this.maxCust - this.minCust)) + this.minCust);
+      console.log(randomCustv);
+      this.randomCust.push(randomCustv);
+    },
+  // cupsPerHourf: function() {
+  //   var cupsPerHourv = this.randomCust[i] * this.averageCups;
+  //   console.log(cupsPerHourv);
+  //   return cupsPerHourv;
+  // },
+  // lbsPerHourf: function() {
+  //   var lbsPerHourv = this.randomCust * averageLbs;
+  //   console.log(lbsPerHour);
+  //   return lbsPerHour;
+  // },
+  // // employeeNumber: function() {
+  // //   var employeeNumber = Math.round()
+  // // }
+  }
+}
+
+
+Pike.randomCustf();
+Pike.cupsPerHourf();
